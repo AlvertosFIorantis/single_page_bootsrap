ecce8f6fc1bb3997f2208cbb2d8db5e7372e5b6f
AlvertosFiorantis <fiorantis-al@hotmail.com>
2020-07-05 19:13:37 +0100

    adding video and sinergates section

---
+++ js/app.js
@@ -0,0 +1,27 @@
+function LivaTranslateUK() {
+  document.getElementById('livaCard').classList.add('rotate-sinergates')
+}
+
+function LivaTranslateGreek() {
+  document.getElementById('livaCard').classList.remove('rotate-sinergates')
+}
+
+function KolivakisTranslateUK() {
+  document.getElementById('kolivakisCard').classList.add('rotate-sinergates')
+}
+
+function KolivakisTranslateGreek() {
+  document.getElementById('kolivakisCard').classList.remove('rotate-sinergates')
+}
+
+function BaziotopoulosTranslateUK() {
+  document
+    .getElementById('BaziotopoulosCard')
+    .classList.add('rotate-sinergates')
+}
+
+function BaziotopoulosTranslateGreek() {
+  document
+    .getElementById('BaziotopoulosCard')
+    .classList.remove('rotate-sinergates')
+}
